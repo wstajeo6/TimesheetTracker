e970e365a317b4d0dec91051b5a1c7cb816cf1a8
iwakeupat6 <kblaaz@gmail.com>
2020-06-08 19:27:51 +0200

    Added MenuBar with VM

---
+++ Source/TimesheetTrackerVM/Menu/MenuBarVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace TimesheetTrackerVM.Menu
+{
+    public class MenuBarVM : BaseVM
+    {
+        private bool isMenuCollapsed = true;
+        public bool IsMenuCollapsed
+        {
+            get => isMenuCollapsed;
+            set => SetField(ref isMenuCollapsed, value);
+        }
+    }
+}
---
+++ Source/TimesheetTrackerWPF/Controls/MenuBarCtrl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using TimesheetTrackerVM.Menu;
+
+namespace TimesheetTrackerWPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for MenuBarCtrl.xaml
+    /// </summary>
+    public partial class MenuBarCtrl : UserControl
+    {
+        #region Private Fields
+
+        private MenuBarVM vm;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public MenuBarCtrl()
+        {
+            InitializeComponent();
+            SubscribeButtonsEvents();
+            DataContext = vm = new MenuBarVM();
+        }
+
+        #endregion Public Constructors
+
+        #region Private Methods
+
+        private void SubscribeButtonsEvents()
+        {
+            //btnOpenMenu.Click += (s, a) => vm.IsMenuCollapsed = false;
+            //btnCloseMenu.Click += (s, a) => vm.IsMenuCollapsed = true;
+        }
+
+        #endregion Private Methods
+    }
+}
