8028a81917527119788fb347b248d168453f2c64
iwakeupat6 <kblaaz@gmail.com>
2020-06-08 19:26:36 +0200

    Added BaseVM

---
+++ Source/TimesheetTrackerVM/BaseVM.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TimesheetTrackerVM
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        #region Public Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Public Events
+
+        #region Public Methods
+
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return;
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        #endregion Public Methods
+    }
+}
