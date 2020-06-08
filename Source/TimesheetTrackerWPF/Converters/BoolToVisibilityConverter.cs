f6fb66e6d8d7f3ddd5fbb4ec12bf2e61398c39a7
iwakeupat6 <kblaaz@gmail.com>
2020-06-08 19:27:03 +0200

    Added BoolToVisibilityConverter

---
+++ Source/TimesheetTrackerWPF/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using TimesheetTrackerWPF.Converters.Generic;
+
+namespace TimesheetTrackerWPF.Converters
+{
+    public sealed class BooleanToVisibilityConverter : GenericBooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() : base(Visibility.Visible, Visibility.Collapsed)
+        {
+        }
+    }
+}
---
+++ Source/TimesheetTrackerWPF/Converters/Generic/GenericBooleanConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TimesheetTrackerWPF.Converters.Generic
+{
+    public class GenericBooleanConverter<T> : IValueConverter
+    {
+        #region Public Constructors
+
+        public GenericBooleanConverter(T trueValue, T falseValue)
+        {
+            True = trueValue;
+            False = falseValue;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public T False { get; set; }
+        public T True { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is T && EqualityComparer<T>.Default.Equals((T)value, True);
+        }
+
+        #endregion Public Methods
+    }
+}
